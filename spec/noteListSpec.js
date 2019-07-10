75ab219d738b748120ff54e4f44a3cd4bce8a90a
Student <student@MAKERSs-MacBook-Pro-53.local>
2019-07-10 14:19:24 +0100

    Console log shows name of the tests

--- spec/noteListSpec.js
+++ spec/noteListSpec.js
@@ -1,22 +1,22 @@
 function testNoteList() {
   var noteList = new NoteList();
-  assert.isTrue(noteList.notes.length === 0, "Testing NoteList has an empty list of notes");
+  assert.isTrue(noteList.notes.length === 0, "TEST PASS: NoteList has an empty list of notes");
 };
 
 testNoteList();
 
 function testNoteListContent() {
   var noteList = new NoteList();
-  assert.isTrue(noteList.currentNotes() === noteList.notes, "Testing NoteList Content");
+  assert.isTrue(noteList.currentNotes() === noteList.notes, "TEST PASS: NoteList Content");
 };
 
 testNoteListContent();
 
 function testNoteListAddNote() {
   var noteList = new NoteList();
-  var string = "string"
+  var string = "string";
   noteList.addNote(string);
-  assert.isTrue(noteList.notes[0] === string, "Testing NoteList can push Note");
+  assert.isTrue(noteList.notes[0] === string, "TEST PASS: NoteList can push Note");
 };
 
 testNoteListAddNote();
--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,13 +1,14 @@
   function testNoteText() {
-  var note = new Note();
-  assert.isTrue(note.text = "My favourite language is Javascript");
+  var text = "Hello there";
+  var note = new Note(text);
+  assert.isTrue(note.text = text, "TEST PASS: Note is instantiated with text");
 };
 
 testNoteText();
 
 function testNoteNotes() {
   var note = new Note();
-  assert.isTrue(note.content() === note.text);
+  assert.isTrue(note.content() === note.text, "TEST PASS: Note's method content returns text");
 };
 
 testNoteNotes();
